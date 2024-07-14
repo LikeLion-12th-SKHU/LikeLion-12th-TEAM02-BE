752728fb2ea172491bd9332d26424c28f66fa800
Kang Duoh <kduoh99@gmail.com>
2024-07-14 19:36:24 +0900

    🎉 Chore: 프로젝트 초기 세팅 (#1)

---
+++ src/main/java/com/skhu/moodfriend/MoodfriendApplication.java
@@ -0,0 +1,13 @@
+package com.skhu.moodfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodfriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodfriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skhu/moodfriend/MoodfriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhu.moodfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodfriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
