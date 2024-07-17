001ab2e81939cafc970b9dfe39751322ed305013
Kang Duoh <kduoh99@gmail.com>
2024-07-17 11:38:55 +0900

    🐛️ Fix: 비밀번호 재확인 필드 추가, 에러 메시지 수정 (#9)

--- src/main/java/com/skhu/moodfriend/app/dto/auth/reqDto/SignUpReqDto.java
+++ src/main/java/com/skhu/moodfriend/app/dto/auth/reqDto/SignUpReqDto.java
@@ -3,8 +3,7 @@ package com.skhu.moodfriend.app.dto.auth.reqDto;
 public record SignUpReqDto(
         String email,
         String password,
-        String name,
-        String loginType,
-        String roleType
+        String confirmPassword,
+        String name
 ) {
 }
