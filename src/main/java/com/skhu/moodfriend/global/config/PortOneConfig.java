71dbcda9576c18e0dc3e328b95fe330f5fc9d90b
Kang Duoh <kduoh99@gmail.com>
2024-07-28 14:14:05 +0900

    🐛 Fix: PortOneConfig 수정 (#40)

--- src/main/java/com/skhu/moodfriend/global/config/PortOneConfig.java
+++ src/main/java/com/skhu/moodfriend/global/config/PortOneConfig.java
@@ -1,19 +1,21 @@
 package com.skhu.moodfriend.global.config;
 
-import lombok.Getter;
+import com.siot.IamportRestClient.IamportClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Getter
 @Configuration
-public class ImpConfig {
-
-    @Value("${imp.code}")
-    private String code;
+public class PortOneConfig {
 
     @Value("${imp.api.key}")
     private String apiKey;
 
     @Value("${imp.api.secret-key}")
-    private String apiSecretKey;
+    private String secretKey;
+
+    @Bean
+    public IamportClient iamportClient() {
+        return new IamportClient(apiKey, secretKey);
+    }
 }
