4f1ca13d23a1b81ee8d4514c09f5206be1ee4e40
Kang Duoh <kduoh99@gmail.com>
2024-08-04 21:57:05 +0900

    🗃 Fix: 상점 초기 데이터 수정 (#58)

--- src/main/java/com/skhu/moodfriend/app/domain/store/Objects.java
+++ src/main/java/com/skhu/moodfriend/app/domain/store/Objects.java
@@ -4,11 +4,18 @@ import lombok.Getter;
 
 @Getter
 public enum Objects {
-    ITEM_ONE("Item One", 1),
-    ITEM_TWO("Item Two", 2),
-    ITEM_THREE("Item Three", 3),
-    ITEM_FOUR("Item Four", 4),
-    ITEM_FIVE("Item Five", 5);
+    TROPICAL_TREE_WINDOW("열대나무 창문", 50),
+    FAN("선풍기", 30),
+    WHITE_SOFA("화이트 소파", 100),
+    POSTER("포스터", 20),
+    CACTUS("선인장", 25),
+    TODAY_DESSERT("오늘의 디저트", 15),
+    BOOK("책", 40),
+    SHOE_RACK("신발장", 35),
+    OCEAN_WALLPAPER("오션 벽지", 80),
+    BEIGE_FLOORING("베이지 바닥재", 70),
+    CHECK_RUG("체크 러그", 60),
+    HEART_CUSHION("하트 쿠션", 30);
 
     private final String name;
     private final int price;
