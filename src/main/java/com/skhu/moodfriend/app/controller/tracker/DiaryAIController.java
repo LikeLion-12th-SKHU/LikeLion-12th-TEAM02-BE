920eb97eafe2f5fbdc2ff99e8fecdf82caee54f2
Kang Duoh <kduoh99@gmail.com>
2024-08-05 15:38:38 +0900

    🔥 Remove: 사용하지 않는 로직 제거 (#58)

--- src/main/java/com/skhu/moodfriend/app/controller/tracker/DiaryAIController.java
+++ src/main/java/com/skhu/moodfriend/app/controller/tracker/DiaryAIController.java
@@ -2,7 +2,6 @@ package com.skhu.moodfriend.app.controller.tracker;
 
 import com.skhu.moodfriend.app.dto.tracker.resDto.DiaryAIResDto;
 import com.skhu.moodfriend.app.service.tracker.DiaryAIDisplayService;
-import com.skhu.moodfriend.app.service.tracker.DiaryAIModifyService;
 import com.skhu.moodfriend.global.template.ApiResponseTemplate;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -21,27 +20,7 @@ import java.time.LocalDate;
 @RequestMapping("/api/v1/diary/ai")
 public class DiaryAIController {
 
-    private final DiaryAIModifyService diaryAIModifyService;
     private final DiaryAIDisplayService diaryAIDisplayService;
-
-    @DeleteMapping("/delete/{createdAt}")
-    @Operation(
-            summary = "사용자 AI 일기 삭제",
-            description = "작성일을 기준으로 사용자 AI 일기를 삭제합니다.",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "사용자 AI 일기 삭제 성공"),
-                    @ApiResponse(responseCode = "403", description = "권한 문제 or 관리자 문의"),
-                    @ApiResponse(responseCode = "404", description = "해당 AI 일기를 찾을 수 없음"),
-                    @ApiResponse(responseCode = "500", description = "서버 문제 or 관리자 문의")
-            }
-    )
-    public ResponseEntity<ApiResponseTemplate<Void>> deleteDiaryAI(
-            @PathVariable LocalDate createdAt,
-            Principal principal) {
-
-        ApiResponseTemplate<Void> data = diaryAIModifyService.deleteDiaryAI(createdAt, principal);
-        return ResponseEntity.status(data.getStatus()).body(data);
-    }
 
     @GetMapping("/display/{createdAt}")
     @Operation(
